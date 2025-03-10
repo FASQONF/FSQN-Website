83c651526370aaa7d7e4c2e98ee52bdfed58efd5
John Doe <johndoe@example.com>
2025-03-10 13:25:38 +0200

    edits lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-"use client";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Suspense } from "react";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/FooterSection/FooterSection";
 import CookieBanner from "@/components/CookieBanner/CookieBanner";
@@ -48,10 +46,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body >
-        
         <Header/>
         <CookieBanner/>
-        
         {children}
         <Footer/>
       </body>
