f49ecd7ba84b8f1c7cdcc4097449301ce418965b
Urlugall <urlugall@gmail.com>
2025-07-21 20:30:41 +0200

    Small Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,13 @@ import { Suspense, useEffect } from "react";
 import styles from "./page.module.css";
 
 import HeroSection from "@/components/HeroSection/HeroSection";
-import PressMentions from "@/components/PressMentions/PressMentions";
 import FeaturesSection from "@/components/FeaturesSection/FeaturesSection";
 import CryptoCards from "@/components/CryptoCards/CryptoCards";
 import StoreToEarn from "@/components/StoreToEarn/StoreToEarn";
-import StayTuned from "@/components/StayTuned/StayTuned";
 import UltimateUX from "@/components/UltimateUserExperience/UltimateUX";
 import TeamSection from "@/components/TeamSection/TeamSection";
 import PartnersSection from "@/components/PartnersSection/PartnersSection";
 import FAQSection from "@/components/FAQSection/FAQSection";
-import Footer from "@/components/FooterSection/FooterSection";
 
 export default function Home() {
   // Handle hash navigation when the page loads
